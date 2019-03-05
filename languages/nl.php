abcaec49e1f925efbcbc3a6e720962f00f6b87c3
Jeroen Dalsem <jdalsem@coldtrick.com>
2019-03-05 15:36:24 +0100

    added: dutch translation

---
+++ languages/nl.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * This file was created by Translation Editor v7.0-beta
+ * On 2019-03-05 15:35
+ */
+
+return array (
+  'item:object:entity_attachment' => 'Entiteit Bijlage',
+  'collection:object:entity_attachment' => 'Entiteit Bijlages',
+  'item:object:entity_attachment:add' => 'Bijlage toevoegen',
+  'entity_attachments:forms:add:type' => 'Selecteer het type bijlage',
+  'entity_attachments:forms:add:type:linked_entity' => 'Gerelateerde content',
+  'entity_attachments:forms:add:type:custom_link' => 'Eigen link',
+  'entity_attachments:forms:add:type:download_link' => 'Download link',
+  'entity_attachments:href' => 'URL',
+);
