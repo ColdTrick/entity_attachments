9fd50bf239d7d877e2f266108581c8bafc34b113
Jerôme Bakker <jeabakker@coldtrick.com>
2022-05-16 14:27:28 +0200

    chore: remove unneeded code

--- classes/EntityAttachment.php
+++ classes/EntityAttachment.php
@@ -15,13 +15,6 @@ class EntityAttachment extends ElggObject {
 	/**
 	 * {@inheritDoc}
 	 */
-	public function canComment($user_guid = 0, $default = null) {
-		return false;
-	}
-
-	/**
-	 * {@inheritDoc}
-	 */
 	public function getURL() {
 		return $this->href ?: parent::getURL();
 	}
